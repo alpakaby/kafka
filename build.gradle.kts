1b675d20af5d8589c2b2b8bc4e86f2e2b32603eb
Anton Shabouta <zloyusr@gmail.com>
2024-04-16 01:53:16 +0300

    WIP

--- build.gradle.kts
+++ build.gradle.kts
@@ -53,7 +53,7 @@ tasks {
         dependsOn.addAll(listOf("compileJava", "compileKotlin", "processResources")) // We need this for Gradle optimization to work
         archiveClassifier.set("alpakaby-kafka") // Naming the jar
         duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-        manifest { attributes(mapOf("Main-Class" to group)) } // Provided we set it up in the application plugin configuration
+        manifest { attributes(mapOf("Main-Class" to "org.alpaka.kafka")) } // Provided we set it up in the application plugin configuration
         val sourcesMain = sourceSets.main.get()
         val contents = configurations.runtimeClasspath.get()
             .map { if (it.isDirectory) it else zipTree(it) } +
