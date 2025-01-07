ba7f37fdfbed0d33ff91b62dd297f0bb6bd40add
Anton Shabouta <zloyusr@gmail.com>
2025-01-07 19:02:23 +0300

    Allow dynamic configuration of uuid properties

--- src/main/kotlin/org/alpaka/kafka/UuidConverter.kt
+++ src/main/kotlin/org/alpaka/kafka/UuidConverter.kt
@@ -3,7 +3,9 @@ package org.alpaka.kafka
 import io.debezium.spi.converter.CustomConverter
 import io.debezium.spi.converter.CustomConverter.ConverterRegistration
 import io.debezium.spi.converter.RelationalColumn
+import org.apache.kafka.common.config.ConfigDef
 import org.apache.kafka.connect.data.SchemaBuilder
+import org.apache.kafka.connect.transforms.util.SimpleConfig
 import org.postgresql.util.PGobject
 import java.nio.ByteBuffer
 import java.util.*
@@ -13,9 +15,22 @@ class UuidConverter: CustomConverter<SchemaBuilder, RelationalColumn> {
         .name("io.debezium.data.Uuid")
         .version(1)
 
-    private val columns = arrayOf("_idrref", "_parentidrref")
+    private lateinit var columns: List<String>
+
+    private val CONFIG_DEF: ConfigDef = ConfigDef()
+        .define(
+            "columns",
+            ConfigDef.Type.LIST,
+            listOf<String>(),
+            ConfigDef.Importance.HIGH,
+            "Name of columns to convert"
+        )
 
     override fun configure(properties: Properties) {
+        val config = SimpleConfig(CONFIG_DEF, properties)
+        val configured = config.getList("columns")
+
+        columns = listOf("_idrref", "_parentidrref") + configured
     }
 
     override fun converterFor(column: RelationalColumn, registration: ConverterRegistration<SchemaBuilder>) {
